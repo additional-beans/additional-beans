a26e561dd945c1488ce56d619f9eba3d33dcd0c1
Yanming Zhou <zhouyanming@gmail.com>
2025-09-02 09:50:22 +0800

    Remove unnecessary `@AutoConfigureAfter` and use `@Import` instead

--- additional-beans-mongodb-spring-boot-starter/src/main/java/io/additionalbeans/mongodb/AdditionalMongodbAutoConfiguration.java
+++ additional-beans-mongodb-spring-boot-starter/src/main/java/io/additionalbeans/mongodb/AdditionalMongodbAutoConfiguration.java
@@ -1,24 +1,13 @@
 package io.additionalbeans.mongodb;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Role;
-
-import static org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Yanming Zhou
  */
 @AutoConfiguration
-@AutoConfigureAfter(MongoAutoConfiguration.class)
+@Import(AdditionalMongodbPostProcessor.class)
 public class AdditionalMongodbAutoConfiguration {
-
-	@Bean
-	@Role(ROLE_INFRASTRUCTURE)
-	static AdditionalMongodbPostProcessor additionalMongodbPostProcessor() {
-		return new AdditionalMongodbPostProcessor();
-	}
 
 }
--- additional-beans-redis-spring-boot-starter/src/main/java/io/additionalbeans/redis/AdditionalRedisAutoConfiguration.java
+++ additional-beans-redis-spring-boot-starter/src/main/java/io/additionalbeans/redis/AdditionalRedisAutoConfiguration.java
@@ -1,24 +1,13 @@
 package io.additionalbeans.redis;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Role;
-
-import static org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Yanming Zhou
  */
 @AutoConfiguration
-@AutoConfigureAfter(RedisAutoConfiguration.class)
+@Import(AdditionalRedisPostProcessor.class)
 public class AdditionalRedisAutoConfiguration {
-
-	@Bean
-	@Role(ROLE_INFRASTRUCTURE)
-	static AdditionalRedisPostProcessor additionalRedisPostProcessor() {
-		return new AdditionalRedisPostProcessor();
-	}
 
 }
